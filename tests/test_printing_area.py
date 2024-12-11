2348ed45381180ecf29f07ff45e357ffa7070d01
ysard <ysard@users.noreply.github.com>
2024-12-11 19:35:18 +0100

    [tests][printing area] Add block to test wrong commands; Cover ESC ( U wrong value

--- tests/test_printing_area.py
+++ tests/test_printing_area.py
@@ -15,6 +15,23 @@ from .helpers.diff_pdf import is_similar_pdfs
 from escparser.parser import ESCParser
 
 
+@pytest.mark.parametrize(
+    "format_databytes",
+    [
+        # ESC ( U - 70/3600 is a not allowed value
+        b"\x1b(U\x01\x00\x46" + cancel_bold,
+    ],
+    # First param goes in the 'databytes' param of the fixture format_databytes
+    indirect=["format_databytes"],
+    ids=[
+        "unit_70/3600",
+    ],
+)
+def test_wrong_commands(format_databytes: bytes):
+    """Test various commands with wrong parameters that will raise a Lark exception"""
+    with pytest.raises(UnexpectedToken, match=r"Unexpected token Token.*"):
+        _ = ESCParser(format_databytes)
+
 
 @pytest.mark.parametrize(
     "format_databytes, expected_unit",
@@ -22,7 +39,6 @@ from escparser.parser import ESCParser
         (b"" + cancel_bold, None),
         (b"\x1b(U\x01\x00\x05" + cancel_bold, 5 / 3600),
         (b"\x1b(U\x01\x00\x14" + cancel_bold, 20 / 3600),
-        # (b"\x1b(U\x01\x00\x46" + cancel_bold, 70 / 3600)
     ],
     # First param goes in the 'request' param of the fixture format_databytes
     indirect=["format_databytes"],
@@ -30,14 +46,12 @@ from escparser.parser import ESCParser
         "unit_default",
         "unit_5/3600",
         "unit_20/3600",
-        # "unit_70/3600",
     ],
 )
 def test_set_unit(format_databytes, expected_unit):
     """Test ESC ( U command
 
     The given value is divided by 3600.
-    TODO: test 70/3600 not allowed value
     """
     escparser = ESCParser(format_databytes, pdf=False)
     assert escparser.defined_unit == expected_unit
