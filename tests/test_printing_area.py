52f0e445639d2c76e9905c5c05a994e51747b952
ysard <ysard@users.noreply.github.com>
2024-12-13 19:44:38 +0100

    [tests] Cover ESC C, ESC ( C

--- tests/test_printing_area.py
+++ tests/test_printing_area.py
@@ -21,11 +21,14 @@ from escparser.parser import ESCParser
     [
         # ESC ( U - 70/3600 is a not allowed value
         b"\x1b(U\x01\x00\x46" + cancel_bold,
+        # Set page length in the current line spacing exceeding 0x7f - ESC C
+        b"\x1bC\x80",
     ],
     # First param goes in the 'databytes' param of the fixture format_databytes
     indirect=["format_databytes"],
     ids=[
         "unit_70/3600",
+        "page_length_lines",
     ],
 )
 def test_wrong_commands(format_databytes: bytes):
@@ -213,3 +216,81 @@ def test_set_page_format(format_databytes, page_size, expected_margins):
     print("Page length:", escparser.page_length)
     assert escparser.top_margin == expected_top_margin
     assert escparser.bottom_margin == expected_bottom_margin
+
+
+@pytest.mark.parametrize(
+    "format_databytes, expected",
+    [
+        # hex(11*360): 0xf78
+        # Send bottom margin 11inch, so 11.69-11 = 0.69: Correct values
+        (b"\x1b(C\x02\x00\x78\x0f", 11),
+        # Prepend ESC ( U to set defined unit to 2 / 360
+        # hex(11*360/2): 0x7bc
+        (b"\x1b(U\x01\x00\x14\x1b(C\x02\x00\xbc\x07", 11),
+        # hex(23*360): 0x2058
+        # Send a 23inch page length: > 22 inch
+        # This value is outside the accepted area, the value will be set to 22.
+        (b"\x1b(C\x02\x00\x58\x20", 22),
+        # Test the reset of top/bottom margins, see test_set_page_format
+        # for the explanations about the value.
+        (b"\x1b(c\x04\x00\x08\x02\x78\x0f\x1b(C\x02\x00\x78\x0f", 11),
+    ],
+    # First param goes in the 'request' param of the fixture format_databytes
+    indirect=["format_databytes"],
+    ids=[
+        "accepted_value",
+        "accepted_value+defined_unit",
+        "23inches",
+        "reset_top_bottom_margins",
+    ],
+)
+def test_set_page_length_defined_unit(format_databytes, expected):
+    """Set page length in defined unit - ESC ( C
+
+    .. note:: default unit is 1 / 360.
+    """
+    escparser = ESCParser(format_databytes, pdf=False)
+    assert escparser.page_length == expected
+    top_margin, bottom_margin = escparser.printable_area[0:2]
+    assert escparser.top_margin == top_margin
+    assert escparser.bottom_margin == bottom_margin
+
+@pytest.mark.parametrize(
+    "format_databytes, expected",
+    [
+        # hex(11*360): 0xf78
+        # Send bottom margin 11inch, so 11.69-11 = 0.69: Correct values
+        (b"\x1bC\x42", 11),
+        # Prepend ESC ( U to set defined unit to 2 / 360
+        # hex(11*72/7): ~113 = 0x71
+        (b"\x1b1C\x71", 11.192913385826774),
+        # hex(23): 0x17
+        # PS: if we keep the current linespacing (1/60), the value for 23inch
+        # is 138, buch is > to the max expected value: 127.
+        # Set the linespacing before to 1inch (60/60) with ESC A.
+        # Send a 23inch page length: > 22 inch
+        # This value is outside the accepted area, the value will be set to 22.
+        (b"\x1bA\x3c\x1bC\x17", 22),
+        # Test the reset of top/bottom margins, see test_set_page_format
+        # for the explanations about the value.
+        (b"\x1b(c\x04\x00\x08\x02\x78\x0f\x1bC\x42", 11),
+    ],
+    # First param goes in the 'request' param of the fixture format_databytes
+    indirect=["format_databytes"],
+    ids=[
+        "accepted_value",
+        "accepted_value+defined_unit",
+        "23inches",
+        "reset_top_bottom_margins",
+    ],
+)
+def test_set_page_length_lines(format_databytes, expected):
+    """Set page length in the current line spacing - ESC C
+
+    .. note:: default linespacing is 1 / 6.
+    """
+    escparser = ESCParser(format_databytes, pdf=False)
+    assert escparser.page_length == expected
+    top_margin, bottom_margin = escparser.printable_area[0:2]
+    assert escparser.top_margin == top_margin
+    assert escparser.bottom_margin == bottom_margin
