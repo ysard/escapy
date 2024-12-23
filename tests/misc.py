cd139391ebc090931b3c8e9c69e67d2c93e6d838
ysard <ysard@users.noreply.github.com>
2024-12-23 03:13:31 +0100

    [tests] Add license

--- tests/misc.py
+++ tests/misc.py
@@ -1,3 +1,19 @@
+#  ESCParser is a software allowing to convert EPSON ESC/P, ESC/P2
+#  printer control language files into PDF files.
+#  Copyright (C) 2024  Ysard
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Common commands, fixtures & functions used in tests"""
 # Standard imports
 import os
