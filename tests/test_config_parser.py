de39ed9b7d29961ceb5fc3fa0ffb039eab47c6ce
ysard <ysard@users.noreply.github.com>
2024-12-29 21:52:14 +0100

    [tests] Cover config parser checks for empty, default, wrong values

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -23,6 +23,7 @@ import configparser
 import pytest
 
 # Local imports
+from escparser.__main__ import build_parser_params
 from escparser.commons import log_level, LOG_LEVEL, DIR_FONTS
 from escparser.config_parser import parse_config, load_config
 
@@ -32,6 +33,9 @@ def default_config():
     misc_section = {
         "loglevel": LOG_LEVEL,
         "default_font_path": DIR_FONTS,
+        "page_size": "A4",
+        "automatic_linefeed": "false",
+        "single_sheets": "true"
     }
 
     roman_section = {
@@ -73,20 +77,111 @@ def test_empty_file():
 
 
 def test_default_file():
-    """Test default config file loading"""
+    """Test the loading of the default config file embedded with the application"""
     sample_config = load_config()
-    misc_section, roman_section = default_config()
+    expected_misc_section, expected_roman_section = default_config()
 
     # Transtype for easier debugging (original object has a different string rep)
     found_misc_section = dict(sample_config["misc"])
     found_roman_section = dict(sample_config["Roman"])
 
-    assert misc_section == found_misc_section
-    assert roman_section == found_roman_section
+    assert found_misc_section == expected_misc_section
+    assert found_roman_section == expected_roman_section
 
 
 @pytest.mark.parametrize(
-    "sample_config,expected_settings",
+    "sample_config",
+    [
+        # Configs that will raise a SystemExit
+        (
+            # sample0:
+            """
+            [misc]
+            pins = 472
+            """
+        ),
+        (
+            # sample1:
+            """
+            [misc]
+            pins = aaa
+            """
+        ),
+        (
+            # sample2:
+            """
+            [misc]
+            printable_area_margins_mm = aaa
+            """
+        ),
+        (
+            # sample3:
+            """
+            [misc]
+            printable_area_margins_mm = 2, 3, 4
+            """
+        ),
+        (
+            # sample4:
+            """
+            [misc]
+            printable_area_margins_mm = 1.0, 2.0, 3.0, a.b
+            """
+        ),
+        (
+            # sample5:
+            """
+            [misc]
+            page_size = A30
+            """
+        ),
+        (
+            # sample6:
+            """
+            [misc]
+            page_size = 3.0
+            """
+        ),
+        (
+            # sample7:
+            """
+            [misc]
+            page_size = a, b
+            """
+        ),
+        (
+            # sample8:
+            """
+            [misc]
+            single_sheets = xxx
+            """
+        ),
+        (
+            # sample9:
+            """
+            [misc]
+            automatic_linefeed = xxx
+            """
+        ),
+    ],
+    ids=[""] * 10,
+)
+def test_erroneous_settings(sample_config):
+    """Test settings that should raise a SystemExit exception with an error msg
+
+    :param sample_config: Tested configuration string that will be parsed.
+    """
+    # PS: can't use the fixture here, the SystemExit will be captured by it
+    # not by the context manager here...
+    config = configparser.ConfigParser()
+    config.read_string(sample_config)
+
+    with pytest.raises(SystemExit):
+        _ = parse_config(config)
+
+
+@pytest.mark.parametrize(
+    "sample_config, expected_settings",
     [
         # Config with user settings vs expected parsed settings
         (
@@ -125,3 +220,76 @@ def test_specific_settings(sample_config, expected_settings):
             assert (
                 sample_config[section][param] == value
             ), f"Fault key: {section}:{param}"
+
+
+@pytest.mark.parametrize(
+    "sample_config, expected",
+    [
+        # Config with user settings vs expected kwargs
+        # All settings have an empty string value
+        (
+            """
+            [misc]
+            loglevel =
+            pins =
+            printable_area_margins_mm =
+            page_size =
+            single_sheets =
+            automatic_linefeed =
+            """,
+            {
+                "pins": None,
+                "printable_area_margins_mm": None,
+                "page_size": (595.2755905511812, 841.8897637795277),
+                "single_sheets": True,
+                "automatic_linefeed": False,
+            }
+        ),
+        # floats for printable_area_margins_mm & page_size
+        (
+            """
+            [misc]
+            loglevel = warning
+            pins = 9
+            printable_area_margins_mm = 6.35, 6.35, 6.35, 6.35
+            page_size = 595.0,841.0
+            single_sheets = false
+            automatic_linefeed = false
+            """,
+            {
+                "pins": 9,
+                "printable_area_margins_mm": (6.35, 6.35, 6.35, 6.35),
+                "page_size": (595.0, 841.0),
+                "single_sheets": False,
+                "automatic_linefeed": False,
+            }
+        ),
+        # ints for printable_area_margins_mm, alias for page_size
+        (
+            """
+            [misc]
+            pins = 48
+            printable_area_margins_mm = 6, 6, 6, 6
+            page_size = A4
+            single_sheets = true
+            automatic_linefeed = true
+            """,
+            {
+                "pins": 48,
+                "printable_area_margins_mm": (6.0, 6.0, 6.0, 6.0),
+                "page_size": (595.2755905511812, 841.8897637795277),
+                "single_sheets": True,
+                "automatic_linefeed": True,
+            }
+        ),
+    ],
+    ids=[""] * 3,
+    indirect=["sample_config"],  # Send sample_config val to the fixture
+)
+def test_build_parser_params(sample_config, expected):
+    """Test legit settings verified by the configparser and prepared as kwargs for ESCParser
+
+    .. seealso:: :meh:`build_parser_params`.
+    """
+    found = build_parser_params(sample_config)
+    assert found == expected
