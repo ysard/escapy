5b68772a3e557474eb5df6a2aac5fdd3ebff5e29
ysard <ysard@users.noreply.github.com>
2026-06-29 20:56:44 +0200

    [config] Add printer profile parser

---
+++ escapy/printer_profile.py
@@ -0,0 +1,147 @@
+#  EscaPy is a software allowing to convert EPSON ESC/P, ESC/P2
+#  printer control language files into PDF files.
+#  Copyright (C) 2024-2026  Ysard
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Printer profile related functions
+
+Class: PrinterProfile
+Functions:
+    - load_printer_profile
+    - get_printer_profile
+"""
+
+# Standard imports
+from pathlib import Path
+from configparser import ConfigParser
+from dataclasses import dataclass
+
+# Custom imports
+from reportlab.lib.colors import PCMYKColorSep
+
+# Local imports
+from escapy.commons import CONFIG_FILES, logger
+
+LOGGER = logger()
+
+
+@dataclass(slots=True)
+class PrinterProfile:
+    """Store various physical attributes defining a printer model"""
+
+    name: str
+    color_names: dict[int, str]
+    RGB_colors: dict[int, str]
+    CMYK_colors: dict[int, PCMYKColorSep]
+    nozzle_offsets: dict[int, float | int]
+    nozzle_offsets_monochrome: dict[int, float | int]
+
+
+def load_printer_profile(config: ConfigParser, profile_dir: Path) -> None:
+    """Read a printer profile file, check and set default values
+
+    :param config: The current configuration that will be updated by the
+        currently selected printer profile. The section `[printer]` and the
+        `profile` key are used to load a specific profile.
+    :param profile_dir: The directory from which the current configuration file
+        has been read. The printer profiles are first searched for in this
+        folder.
+    """
+    # First, search for profiles in the same folder as the currently used
+    # configuration file;
+    # Then, search for in standard folders.
+    dirs = [profile_dir, profile_dir / "profiles"] + [
+        file.parent / "profiles" for file in CONFIG_FILES
+    ]
+
+    profile_name = config.get("printer", "profile", fallback="generic")
+    LOGGER.debug("Expect the printer profile: %s in %s", profile_name, dirs)
+
+    # Default profile
+    config.read([d / "generic.ini" for d in dirs])
+    if profile_name == "generic":
+        return
+
+    # More specific values override the previous ones
+    profile_path_found = config.read([d / f"{profile_name}.ini" for d in dirs])
+    if not profile_path_found:
+        LOGGER.error("Printer profile was not found: %s", profile_name)
+    else:
+        LOGGER.debug("Use the printer profile at <%s>", profile_path_found[0])
+
+
+def get_printer_profile(config: ConfigParser) -> PrinterProfile:
+    """Build printer color profile from the given config"""
+    color_names = {}
+    RGB_colors = {}
+    CMYK_colors = {}
+    nozzle_offsets = {}
+    nozzle_offsets_monochrome = {}
+
+    colors_section = config["colors"]
+
+    for color_id_str, logical_name in colors_section.items():
+
+        section = f"color:{logical_name}"
+        if not config.has_section(section):
+            LOGGER.error(
+                "Color <%s:%s> is not available in the profile!",
+                color_id_str,
+                logical_name,
+            )
+            continue
+
+        color_id = int(color_id_str, 0)
+
+        # Monochrome mode: search an eventual color definition
+        mono = f"{section}:mono"
+        if config.has_section(mono):
+            offset = config.getint(mono, "offset", fallback=0)
+            nozzle_offsets_monochrome[color_id] = offset / 180
+
+        # Color mode
+        display = config.get(
+            section,
+            "display",
+            fallback=logical_name.replace("_", " ").title(),
+        )
+
+        offset = config.getint(section, "offset", fallback=0)
+
+        cmyk = tuple(int(x.strip()) for x in config.get(section, "cmyk").split(","))
+
+        rgb = config.get(section, "rgb")
+
+        color_names[color_id] = display
+
+        nozzle_offsets[color_id] = offset / 180
+
+        CMYK_colors[color_id] = PCMYKColorSep(
+            *cmyk,
+            spotName=display.upper(),
+        )
+
+        RGB_colors[color_id] = rgb
+
+    # Note: inherit all offsets from the color mode
+    nozzle_offsets_monochrome = nozzle_offsets | nozzle_offsets_monochrome
+
+    return PrinterProfile(
+        name=config.get("printer", "profile", fallback="generic"),
+        color_names=color_names,
+        RGB_colors=RGB_colors,
+        CMYK_colors=CMYK_colors,
+        nozzle_offsets=nozzle_offsets,
+        nozzle_offsets_monochrome=nozzle_offsets_monochrome,
+    )
